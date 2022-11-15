835d643d863f8e0fc58c69f6d738b161de111c16
sanggur591 <115924970+sanggur591@users.noreply.github.com>
2022-11-16 00:41:45 +0900

    Update Solution.java

--- level0/컨트롤 제트/Solution.java
+++ level0/컨트롤 제트/Solution.java
@@ -6,17 +6,17 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         String[] arr = s.split(" ");//문자열 쪼개주기
-        ArrayList<String> arraylist1 = new ArrayList<>(Arrays.asList(arr));
-        ArrayList<String> arraylist2 = new ArrayList<>();
+        ArrayList<String> list1 = new ArrayList<>(Arrays.asList(arr));
+        LinkedList<String> list2 = new LinkedList<>();
         for(int i=0;i<arr.length;i++){//list1에서 list2로 Z가 아니면 넣어주고 Z면 list2의 마지막요소 삭제
-            if(arraylist1.get(i).equals("Z")){
-                if(arraylist2.size()!=0){
-                    arraylist2.remove(arraylist2.size()-1);
+            if(list1.get(i).equals("Z")){
+                if(list2.size()!=0){
+                    list2.remove(list2.size()-1);
                 }
             }
-            else arraylist2.add(arraylist1.get(i));
+            else list2.add(list1.get(i));
         }
-        Iterator<String> iterator = arraylist2.iterator();//합계구하기
+        Iterator<String> iterator = list2.iterator();//합계구하기
         while(iterator.hasNext()){
             String x = iterator.next();
             answer += Integer.parseInt(x);
